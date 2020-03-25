2958a45967e484974aceeba13d6ef9a1fa067ace
Turzhanskyi <turzhansky81@gmail.com>
2020-03-25 11:13:02 +0200

    episode 17: Arrays

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learn Ruby screencasts
+
+RubyScreencastsRu: https://www.youtube.com/watch?v=IWZ_71EKbng&list=PLKMUE6-VEbgH3bKy1K0wnWw0AqzgVdGRM&index=6&t=0s
---
+++ storeapp/cart.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Cart
+
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+
+  def add_item(item)
+    @items.push item
+  end
+
+  def remove_item
+    @items.pop
+  end
+
+end
