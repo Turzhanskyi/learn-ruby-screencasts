2ad1364d87b2b4db4d2a4af6e2f5229684b05a54
Turzhanskyi <turzhansky81@gmail.com>
2020-03-26 15:17:36 +0200

    episode 20: Blocks, part 3

--- storeapp/cart.rb
+++ storeapp/cart.rb
@@ -17,5 +17,9 @@ class Cart
 
   def validate
     @items.each { |i| puts 'Item has no price' if i.price.nil? }
+    end
+
+  def delete_invalid_items
+    @items.delete_if { |i| i.price.nil? }
   end
 end
