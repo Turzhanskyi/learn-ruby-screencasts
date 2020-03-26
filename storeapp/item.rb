acf4545bc3dc07a08365d8e1daeacb54dcdfb9b7
Turzhanskyi <turzhansky81@gmail.com>
2020-03-26 14:42:14 +0200

    episode 19: Blocks, part 2

--- storeapp/item.rb
+++ storeapp/item.rb
@@ -4,8 +4,15 @@ class Item
   def initialize(options = {})
     @price = options[:price]
     @weight = options[:weight]
+    @name = options[:name]
   end
 
-  attr_reader :price, :weight
+  attr_reader :price, :weight, :name
   attr_writer :price
+
+  def info
+    yield(name)
+    yield(weight)
+    yield(price)
+  end
 end
