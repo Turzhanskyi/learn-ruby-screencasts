9e20ddd2fbdd5e4e591fc68903979b20c771889d
Turzhanskyi <turzhansky81@gmail.com>
2020-03-26 15:35:56 +0200

    episode 21: Inheritance and Polymorphism

--- storeapp/item.rb
+++ storeapp/item.rb
@@ -3,16 +3,14 @@
 class Item
   def initialize(options = {})
     @price = options[:price]
-    @weight = options[:weight]
     @name = options[:name]
   end
 
-  attr_reader :price, :weight, :name
+  attr_reader :price, :name
   attr_writer :price
 
   def info
     yield(name)
-    yield(weight)
     yield(price)
   end
 end
