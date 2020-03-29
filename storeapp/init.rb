7e980f7d36486ecca19c0534c432ed20e98daf6c
Turzhanskyi <turzhansky81@gmail.com>
2020-03-29 23:29:23 +0300

    episode 35: Syntax sugar

--- storeapp/init.rb
+++ storeapp/init.rb
@@ -10,23 +10,14 @@ require_relative 'cart'
 require_relative 'order'
 
 @items = []
-@items << AntiqueItem.new({ price: 101, weight: 100, name: 'car' })
-@items << RealItem.new({ price: 101, weight: 100, name: 'kettle' })
-@items << RealItem.new({ price: 101, weight: 100, name: 'dishwasher' })
+@items << AntiqueItem.new('car', price: 101, weight: 100)
+@items << RealItem.new(price: 101, weight: 100, name: 'kettle')
+@items << RealItem.new(price: 101, weight: 100, name: 'dishwasher')
 
 cart = Cart.new('roman')
-cart.add_item RealItem.new({ price: 101, weight: 100, name: 'car' })
-cart.add_item RealItem.new({ price: 150, weight: 100, name: 'car' })
-cart.add_item RealItem.new({ price: 120, weight: 100, name: 'kettle' })
+cart.add_item RealItem.new(price: 101, weight: 100, name: 'car')
+cart.add_item RealItem.new(price: 150, weight: 100, name: 'car')
+cart.add_item RealItem.new(price: 120, weight: 100, name: 'kettle')
 
-puts @items[0].is_a?(AntiqueItem)
-puts @items[0].is_a?(Item)
-
-puts @items[0].class == AntiqueItem
-puts @items[0].class == Item
-
-puts @items[0].respond_to?(:info)
-puts @items[0].respond_to?(:important_info)
-
-puts @items[0].send(:tax)
-puts @items[0].tax
+p cart.all_cars
+p cart.all_kettle
