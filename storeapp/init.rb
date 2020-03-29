1d190f91588060fa7b7f2c26ed864351bbaffd4c
Turzhanskyi <turzhansky81@gmail.com>
2020-03-29 18:19:48 +0300

    episode 33: Reflection: method missing

--- storeapp/init.rb
+++ storeapp/init.rb
@@ -3,8 +3,8 @@
 require_relative 'string'
 require_relative 'item_container'
 require_relative 'item'
-require_relative 'real_item'
 require_relative 'virtual_item'
+require_relative 'real_item'
 require_relative 'antique_item'
 require_relative 'cart'
 require_relative 'order'
@@ -13,3 +13,11 @@ require_relative 'order'
 @items << AntiqueItem.new({ price: 101, weight: 100, name: 'car' })
 @items << RealItem.new({ price: 101, weight: 100, name: 'kettle' })
 @items << RealItem.new({ price: 101, weight: 100, name: 'dishwasher' })
+
+cart = Cart.new('roman')
+cart.add_item RealItem.new({ price: 101, weight: 100, name: 'car' })
+cart.add_item RealItem.new({ price: 150, weight: 100, name: 'car' })
+cart.add_item RealItem.new({ price: 120, weight: 100, name: 'kettle' })
+
+p cart.all_cars
+p cart.all_kettles
--- storeapp/item_container.rb
+++ storeapp/item_container.rb
@@ -8,7 +8,15 @@ module ItemContainer
   end
 
   module InstanceMethods
-    def  add_item(item)
+    def method_missing(method_name)
+      if method_name =~ /^all_/
+        show_all_items_with_name(method_name.to_s.sub(/^all_/, '').chomp('s'))
+      else
+        super
+      end
+    end
+
+    def add_item(item)
       @items.push item unless item.price < self.class.min_price
     end
 
@@ -26,6 +34,12 @@ module ItemContainer
 
     def count_valid_items
       @items.count(&:price)
+    end
+
+    private
+
+    def show_all_items_with_name(n)
+      @items.map { |i| i if n == i.name }.delete_if(&:nil?)
     end
   end
 
