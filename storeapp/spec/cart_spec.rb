57d3090d209f6400004c4334786ab0fa5740dad0
Turzhanskyi <turzhansky81@gmail.com>
2020-03-30 20:00:24 +0300

    episode 43: Test Driven Development

---
+++ storeapp/spec/cart_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+require_relative '../app/item'
+require_relative '../app/virtual_item'
+require_relative '../app/real_item'
+require_relative '../app/antique_item'
+require_relative '../app/item_container'
+require_relative '../app/cart'
+
+describe Cart do
+  it 'adds items into the cart' do
+    cart = Cart.new('vitalii')
+    item1 = Item.new('kettle', price: 200)
+    item2 = Item.new('car', price: 200)
+    cart.add_items(item1, item2)
+    expect(cart.items).to include(item1, item2)
+  end
+end
